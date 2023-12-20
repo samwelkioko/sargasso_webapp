14a3fdfe74f5bc320dd80538bfd8d42f3a605a32
SAMWEL KIOKO KITAKA <samwelkioko@gmail.com>
2023-12-20 21:01:03 +0300

    upload

--- Sargassoweb/settings.py
+++ Sargassoweb/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -130,6 +131,12 @@ MEDIA_ROOT = BASE_DIR / "media_cdn"
 STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
 STATIC_URL = STATIC_HOST + "/static/"
 
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
